f2934d2b37d8d42a0b909dee0744fa1862f7fc1d
27jorg16 <146002952+27jorg16@users.noreply.github.com>
2024-05-01 23:10:12 -0500

    Delete main directory

--- main/java/pe/edu/cibertec/TiendaGranRitmo/repository/InstrumentoRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.cibertec.TiendaGranRitmo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.cibertec.TiendaGranRitmo.model.bd.Instrumento;
-
-@Repository
-public interface InstrumentoRepository extends JpaRepository<Instrumento,Integer> {
-}
--- main/java/pe/edu/cibertec/TiendaGranRitmo/repository/VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.cibertec.TiendaGranRitmo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.cibertec.TiendaGranRitmo.model.bd.Venta;
-
-@Repository
-public interface VentaRepository extends JpaRepository<Venta,Integer> {
-}
